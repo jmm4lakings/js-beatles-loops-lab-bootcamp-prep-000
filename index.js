b2320c30b63eea1e5bb2801432f5907952ef9b1c
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-06 20:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ funtion theBeatlesPlay(musicians, instruments) {
       }
       return array; 
 }
+unction johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number += 1
+  } while (number < 15);
+  return array;
+} 
