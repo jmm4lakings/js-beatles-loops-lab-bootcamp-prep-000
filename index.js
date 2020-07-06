75411a4f4eeb9fdf7f940e397f56434caeffb2eb
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-06 20:28:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+funtion theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicains[i] + "plays" + instruments[i]);
+      }
+      return array; 
+}
